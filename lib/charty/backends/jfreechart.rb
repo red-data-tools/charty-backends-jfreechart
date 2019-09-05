a861cee40a28ae88659d7abcea35bac3a747cbf0
284km <k.furuhashi10@gmail.com>
2019-09-05 09:03:20 +0700

    bundle gem -t minitest charty-backends-jfreechart

---
+++ lib/charty/backends/jfreechart.rb
@@ -0,0 +1,10 @@
+require "charty/backends/jfreechart/version"
+
+module Charty
+  module Backends
+    module Jfreechart
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/charty/backends/jfreechart/version.rb
@@ -0,0 +1,7 @@
+module Charty
+  module Backends
+    module Jfreechart
+      VERSION = "0.1.0"
+    end
+  end
+end
