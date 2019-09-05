fd54dfc69a6841afc17a299d2683df6abdcd60fa
284km <k.furuhashi10@gmail.com>
2019-09-05 09:18:37 +0700

    Use test-unit

--- test/charty/backends/jfreechart_test.rb
+++ test/charty/backends/jfreechart_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class Charty::Backends::JfreechartTest < Minitest::Test
+class Charty::Backends::JfreechartTest < Test::Unit::TestCase
   def test_that_it_has_a_version_number
     refute_nil ::Charty::Backends::Jfreechart::VERSION
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "charty/backends/jfreechart"
 
-require "minitest/autorun"
+require "test/unit"
